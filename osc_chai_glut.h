38c2f9135aa6fcd53ffffffd16d350e5ab640920
Stephen Sinclair <sinclair@music.mcgill.ca>
2007-02-19 22:42:09 +0000

    Added a real-time queue mechanism to sit between OSC, CHAI, and ODE threads, which still needs to be put into use.

--- osc_chai_glut.h
+++ osc_chai_glut.h
@@ -40,4 +40,50 @@ extern int lock_world;
 #define LOCK_WORLD() printf("Locking.\n");(lock_world=1)
 #define UNLOCK_WORLD() printf("Unlocking.\n");(lock_world=0)
 
+// Messaging system, operation requests on ODE and CHAI spaces
+typedef enum {
+    ODE_OBJECT_ADD,
+    ODE_OBJECT_REMOVE
+} ode_request_t;
+
+typedef enum {
+    CHAI_OBJECT_ADD,
+    CHAI_OBJECT_REMOVE
+} chai_request_t;
+
+// Request structure
+class request {
+  public:
+    request() { handled = false; }
+    bool handled;
+};
+
+// Request structures for each queue
+class ode_request_class : public request {
+  public:
+    ode_request_class() : request() {}
+    ode_request_t req;
+    cODEPrimitive *ob;
+};
+
+class chai_request_class : public request {
+  public:
+    chai_request_class() : request() {}
+    chai_request_t req;
+    cGenericObject *ob;
+};
+
+// called from OSC thread, non-blocking
+ode_request_class*  post_ode_request(ode_request_t req, cODEPrimitive *ob);
+chai_request_class* post_chai_request(chai_request_t req, cGenericObject *ob);
+
+// called from OSC thread
+void wait_ode_request(ode_request_t req, cODEPrimitive *ob);
+void wait_chai_request(chai_request_t req, cGenericObject *ob);
+
+// called from respective ODE or CHAI thread
+// return non-zero if requests remain in the queue
+int ode_poll_requests();
+int chai_poll_requests();
+
 #endif // _OSC_CHAI_GLUT_H_
