89bdb04f4d769a51b7552a18482013f1d156336c
sinclairs <sinclairs>
2007-03-22 00:12:35 +0000

    Support for Darwin!

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,15 +12,14 @@ liblo_URL=http://easynews.dl.sourceforge.net/sourceforge/liblo/liblo-0.23.tar.gz
 liblo_TAR=liblo-0.23.tar.gz
 liblo_DIR=liblo-0.23
 liblo_MD5=e14c9f4fae7ed8d9622d126f6fb9c1d7
-liblo_PATCH=liblo-0.23-msvc7.patch
 
-if [ $(md5sum $liblo_TAR | cut -d" " -f1)x != ${liblo_MD5}x ]; then
+if [ $($MD5 $liblo_TAR | $MD5CUT)x != ${liblo_MD5}x ]; then
 	echo Downloading $liblo_TAR ...
 	rm -v $liblo_TAR
-	wget -O $liblo_TAR $liblo_URL
+    $DL $liblo_TAR $liblo_URL
 fi
 
-if [ $(md5sum $liblo_TAR | cut -d" " -f1)x != ${liblo_MD5}x ]; then
+if [ $($MD5 $liblo_TAR | $MD5CUT)x != ${liblo_MD5}x ]; then
 	echo "Error in MD5 checksum for $liblo_TAR"
 	exit
 fi
@@ -32,19 +31,43 @@ if !(tar -xzf $liblo_TAR); then
 	exit
 fi
 
+if [ ${liblo_PATCH}x != x ]; then
 echo Patching $liblo_DIR
 if !(cd $liblo_DIR && patch -p1 <../$liblo_PATCH); then
-	echo "Error applying MSVC7 patch."
+	echo "Error applying patch" $liblo_PATCH
 	exit
 fi
+fi
+
+case $(uname) in
+   CYGWIN*)
+   ;;
+   *)
+echo Configuring $liblo_DIR
+if !(cd $liblo_DIR && ./configure --disable-shared); then
+	echo "Error configuring $liblo_DIR"
+	exit
+fi
+
+echo Compiling $liblo_DIR
+if !(cd $liblo_DIR && make); then
+	echo "Error compiling $liblo_DIR"
+	exit
+fi
+    ;;
+esac
 fi
 
 echo
 echo LibLo Done.
 echo
+case $(uname) in
+   CYGWIN*)
 echo Now open solution file "$(cygpath -w $liblo_DIR/LibLo.sln)" in \
     Visual Studio 2003 and Build All.
 explorer "$(cygpath -w $liblo_DIR)"
+   ;;
+esac
 }
 
 ode() {
@@ -52,15 +75,18 @@ ode_URL=http://internap.dl.sourceforge.net/sourceforge/opende/ode-src-0.7.zip
 ode_TAR=ode-src-0.7.zip
 ode_DIR=ode-0.7
 ode_MD5=b6727fef2cbb9ca812438bb774c9d6ec
-#ode_PATCH=ode-0.23-msvc7.patch
+case $(uname) in
+	CYGWIN*)
+    ;;
+esac
 
-if [ $(md5sum $ode_TAR | cut -d" " -f1)x != ${ode_MD5}x ]; then
+if [ $($MD5 $ode_TAR | $MD5CUT)x != ${ode_MD5}x ]; then
 	echo Downloading $ode_TAR ...
 	rm -v $ode_TAR
-	wget -O $ode_TAR $ode_URL
+	$DL $ode_TAR $ode_URL
 fi
 
-if [ $(md5sum $ode_TAR | cut -d" " -f1)x != ${ode_MD5}x ]; then
+if [ $($MD5 $ode_TAR | $MD5CUT)x != ${ode_MD5}x ]; then
 	echo "Error in MD5 checksum for $ode_TAR"
 	exit
 fi
@@ -99,25 +125,87 @@ echo ODE Done.
 echo
 }
 
+chai3d() {
+chai_URL=http://chai3d.org/builds/chai3d%5Bv1.51%5D.zip
+chai_TAR=chai3d\[v1.51\].zip
+chai_MD5=006c5464dc3c389d87e615c24ccd9696
+
+if [ $($MD5 "$chai_TAR" | $MD5CUT)x != ${chai_MD5}x ]; then
+    echo Downloading $chai_TAR ...
+    rm -v $chai_TAR
+    $DL "$chai_TAR" $chai_URL
+fi
+
+if [ $($MD5 "$chai_TAR" | $MD5CUT)x != ${chai_MD5}x ]; then
+    echo "Error in MD5 checksum for $chai_TAR"
+    exit
+fi
+
+if ! [ -d $chai_DIR ]; then
+echo Extracting "$chai_TAR" ...
+if !(unzip -o "$chai_TAR"); then
+    echo "Error in archive.";
+    exit
+fi
+
+if [ ${chai_PATCH}x != x ]; then
+echo Patching $chai_DIR
+if !(patch -p0 <$chai_PATCH); then
+	echo "Error applying patch" $chai_PATCH
+	exit
+fi
+fi
+
+echo Compiling $chai_DIR
+if !(cd $chai_DIR && make); then
+    echo "Error compiling $chai_DIR"
+    exit
+fi
+fi
+
+echo
+echo Chai3d Done.
+echo
+}
 
 # System-dependant bootstrapping
 case $(uname) in
     CYGWIN*)
+	DL="wget -O"
+    MD5=md5sum
+	MD5CUT='cut -d\\  -f1'
+    liblo_PATCH=liblo-0.23-msvc7.patch
+    ode_PATCH=ode-0.23-msvc7.patch
 #    ode
     liblo
     ;;
+
     Linux*)
+	DL="wget -O"
+    MD5=md5sum
+	MD5CUT='cut -d\\  -f1'
+	chai_DIR=chai3d/linux
+
 	ode
     echo For Linux, please get package \"liblo\" from your distribution.
     echo Ubuntu and Debian: sudo apt-get install liblo0-dev
     exit
+	;;
+
+	Darwin*)
+	DL="curl -o"
+    MD5=md5
+	MD5CUT="cut -f2 -d="
+    chai_PATCH=chai3d-1.51-darwin.patch
+	chai_DIR=chai3d/darwin
+    ode
+	liblo
+	chai3d
     ;;
     *)
     echo Your system is not supported by this script.
-    echo Please acquire the \"liblo\" package manually.
+    echo Please acquire the \"liblo\" and \"ode\" packages manually.
     exit
     ;;
 esac
-
-
 
