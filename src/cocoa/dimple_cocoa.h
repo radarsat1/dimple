7bd9772a235aa549ed41be79be515f9694a80836
sinclairs <sinclairs>
2007-05-03 15:00:54 +0000

    Preliminary support for native Cocoa interface. (not yet working, but proven to be possible)

---
+++ src/cocoa/dimple_cocoa.h
@@ -0,0 +1,10 @@
+/*
+ *  dimple_cocoa.h
+ *  dimple_cocoa
+ *
+ *  Created by Stephen Sinclair on 5/3/07.
+ *  Copyright 2007 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+int dimple_cocoa(int argc, char *argv[]);
