afc2976ba33506d9479bc4f8e2b67c3985dee9d3
sinclairs <sinclairs>
2007-05-17 00:59:25 +0000

    cVector3d references must be const to compile with temporary variables in gcc.

--- src/CODEPrimitive.h
+++ src/CODEPrimitive.h
@@ -86,13 +86,13 @@ class cODEPrimitive
 	  //! Update the position of the dynamic object.
 	  void updateDynamicPosition();
 	  //! Set the position of the dynamic object.
-	  void setDynamicPosition(cVector3d &a_pos);
+	  void setDynamicPosition(const cVector3d &a_pos);
 	  //! Set the mass of the dynamic object.
 	  void setDynamicMass(float a_mass);
 	  //! Set a force on the dynamic object.
-      void setDynamicForce(cVector3d &a_force);
+      void setDynamicForce(const cVector3d &a_force);
       //! Set the dynamic object's rotational position.
-      void setDynamicRotation(cMatrix3d &a_rot);
+      void setDynamicRotation(const cMatrix3d &a_rot);
 	  //! Sync the pose of the ODE object with the pose of the CHAI object
 	  void syncPose();
       //! Set data associated with ODE geom
