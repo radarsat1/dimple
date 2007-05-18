4d36ac9a6494154f0e82733df78508213368ba63
Stephen Sinclair <sinclair@music.mcgill.ca>
2007-05-18 17:38:12 +0000

    Classes for sending values at timed intervals.

---
+++ src/valuetimer.h
@@ -0,0 +1,45 @@
+// -*- mode:c++; indent-tabs-mode:nil; c-basic-offset:4; compile-command:"scons debug=1" -*-
+//======================================================================================
+/*
+    This file is part of DIMPLE, the Dynamic Interactive Musically PhysicaL Environment,
+
+    This code is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License("GPL") version 2
+    as published by the Free Software Foundation.  See the file LICENSE
+    for more information.
+
+    sinclair@music.mcgill.ca
+    http://www.music.mcgill.ca/~sinclair/content/dimple
+*/
+//======================================================================================
+
+#ifndef _VALUETIMER_H_
+#define _VALUETIMER_H_
+
+#include "OscObject.h"
+#include "dimple.h"
+
+struct ValueTimerPair
+{
+    int current_ms;
+    int interval_ms;
+};
+
+class ValueTimer
+{
+  public: 
+    ValueTimer() {};
+    virtual ~ValueTimer() {};
+
+    int addValue(OscValue* oscval, int interval_ms);
+    int removeValue(OscValue* oscval);
+
+    void onTimer(int interval_ms);
+
+  protected:
+    std::map<OscValue*, struct ValueTimerPair*> m_values;
+};
+
+extern ValueTimer valuetimer;
+
+#endif // _VALUETIMER_H_
