c7fbebed5f810fb5f4b67c5f9527385dae335649
sinclairs <sinclairs>
2007-05-18 17:38:12 +0000

    Classes for sending values at timed intervals.

---
+++ src/valuetimer.cpp
@@ -0,0 +1,60 @@
+// -*- mode:c++; indent-tabs-mode:nil; c-basic-offset:4; compile-command:"scons debug=1" -*-
+//======================================================================================
+/*
+    This file is part of DIMPLE, the Dynamic Interactive Musically PhysicaL Environment,
+
+    This code is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License("GPL") version 2
+    as published by the Free Software Foundation.  See the file LICENSE
+    for more information.
+
+    sinclair@music.mcgill.ca
+    http://www.music.mcgill.ca/~sinclair/content/dimple
+*/
+//======================================================================================
+
+#include "valuetimer.h"
+
+typedef std::map<OscValue*, struct ValueTimerPair*>::iterator value_iter;
+typedef std::pair<OscValue*, struct ValueTimerPair*> value_pair;
+ValueTimer valuetimer;
+
+
+int ValueTimer::addValue(OscValue* oscval, int interval_ms)
+{
+    value_iter it;
+    it = m_values.find(oscval);
+    if (it==m_values.end()) {
+        struct ValueTimerPair *v = new struct ValueTimerPair;
+        v->current_ms = v->interval_ms = interval_ms;
+        m_values.insert(value_pair(oscval, v));
+    }
+    else {
+        it->second->current_ms = it->second->interval_ms = interval_ms;
+    }
+}
+
+int ValueTimer::removeValue(OscValue* oscval)
+{
+    value_iter it;
+    it = m_values.find(oscval);
+    if (it!=m_values.end()) {
+        delete it->second;
+        m_values.erase(it);
+    }
+}
+    
+void ValueTimer::onTimer(int interval_ms)
+{
+    value_iter it;
+    for (it=m_values.begin(); it!=m_values.end(); it++)
+    {
+        struct ValueTimerPair *v;
+        v = it->second;
+        v->current_ms -= interval_ms;
+        if (v->current_ms <= 0) {
+            v->current_ms = v->interval_ms;
+            it->first->send();
+        }
+    }
+}
