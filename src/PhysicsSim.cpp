508c2c2f0398f881e24ef17e2dc16a54d3e99bb2
Stephen Sinclair <sinclair@music.mcgill.ca>
2007-11-01 22:42:30 -0400

    Physics simulation aggregate should have been included previously.

---
+++ src/PhysicsSim.cpp
@@ -0,0 +1,13 @@
+// -*- mode:c++; indent-tabs-mode:nil; c-basic-offset:4; compile-command:"scons debug=1" -*-
+
+#include "PhysicsSim.h"
+
+PhysicsSim::PhysicsSim(const char *port)
+    : Simulation(port)
+{
+}
+
+PhysicsSim::~PhysicsSim()
+{
+}
+
