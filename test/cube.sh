45c5ef7e8cdf45d9ef408a38b48e648bbbc80256
Stephen Sinclair <sinclair@music.mcgill.ca>
2008-05-14 23:59:59 -0400

    Make cube.sh listen on port 7775.

--- test/cube.sh
+++ test/cube.sh
@@ -7,10 +7,10 @@
 
 # This script assumes Dimple is already running.
 
-# Listen on port 7770.  We'll assume this is the only oscdump instance
+# Listen on port 7775.  We'll assume this is the only oscdump instance
 # running, and we don't want to run it if it's already running in
 # another terminal.
-if ! ( ps -A | grep oscdump >/dev/null 2>&1 ); then (oscdump 7770 &); fi
+if ! ( ps -A | grep oscdump >/dev/null 2>&1 ); then (oscdump 7775 &); fi
 
 oscsend localhost 7774 /world/clear
 oscsend localhost 7774 /world/prism/create sfff cube 0 0 0
