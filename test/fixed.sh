a4e7833cd33fe40b595d4e33d66a779d6c27f583
Stephen Sinclair <sinclair@music.mcgill.ca>
2008-05-19 14:02:54 -0400

    Add a test script for fixed constraints.

---
+++ test/fixed.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# This test file relies on the programs 'oscdump' and 'oscsend' which
+# are available as part of the LibLo distribution.  Currently they are
+# present in the LibLo svn repository, but not yet part of a stable
+# release.
+
+# This script assumes Dimple is already running.
+
+# Listen on port 7775.  We'll assume this is the only oscdump instance
+# running, and we don't want to run it if it's already running in
+# another terminal.
+if ! ( ps -A | grep oscdump >/dev/null 2>&1 ); then (oscdump 7775 &); fi
+
+oscsend localhost 7774 /world/clear
+oscsend localhost 7774 /world/sphere/create sfff s -0.1 0 0
+oscsend localhost 7774 /world/prism/create sfff p 0.1 0 0
+oscsend localhost 7774 /world/s/radius f 0.03
+oscsend localhost 7774 /world/p/size fff 0.06 0.06 0.06
+
+oscsend localhost 7774 /world/fixed/create sss c1 p world
+
+# Push the sphere toward the prism.
+oscsend localhost 7774 /world/s/force fff 0.01 0 0
