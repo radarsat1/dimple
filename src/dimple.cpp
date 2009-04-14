6519e898fa1286dac69ef7382a784805148fb6a4
Stephen Sinclair <sinclair@music.mcgill.ca>
2009-04-14 02:34:48 -0400

    Add a command-line option for the sending OSC address, using getopt.

--- src/dimple.cpp
+++ src/dimple.cpp
@@ -18,6 +18,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <getopt.h>
 
 #include "dimple.h"
 
@@ -32,11 +33,59 @@ int visual_timestep_ms = (int)((1.0/visual_fps)*1000.0);
 int physics_timestep_ms = 10;
 int haptics_timestep_ms = 1;
 
-const char *address_send_host = "localhost";
-const char *address_send_port = "7775";
+char *address_send_url = "osc.udp://localhost:7775";
 
 lo_address address_send;
 int quit = 0;
+
+void help()
+{
+    printf("Usage: dimple [options], where options are:\n\n"
+        "--send-url (-u)     A LibLo-style URL specifying the address\n"
+        "                    to send OSC messages to.\n"
+        "                    Example: osc.udp://localhost:9000\n"
+        "                    Other protocols may be 'tcp', 'unix'.\n"
+        );
+}
+
+void parse_command_line(int argc, char* argv[])
+{
+    int c=0;
+
+    struct option long_options[] = {
+        { "help",     no_argument,       0, 'h' },
+        { "send-url", optional_argument, 0, 'u' },
+        {0, 0, 0, 0}
+    };
+
+    while (c!=-1) {
+        int option_index = 0;
+
+        c = getopt_long (argc, argv, "hu:",
+                         long_options, &option_index);
+
+        switch (c) {
+        case 'u':
+            if (optarg==0) {
+                printf("Error parsing --send-url option.\n");
+                exit(1);
+            }
+            address_send_url = optarg;
+            break;
+        case 'h':
+            help();
+            exit(0);
+            break;
+        case '?':
+            exit(1);
+            break;
+        case -1:
+            break;
+        default:
+            exit(1);
+        }
+    }
+}
 
 void sighandler_quit(int sig)
 {
@@ -63,9 +112,15 @@ int main(int argc, char* argv[])
 
 #ifndef FLEXT_SYS
 	 signal(SIGINT, sighandler_quit);
+     parse_command_line(argc, argv);
 #endif
 
-     address_send = lo_address_new(address_send_host, address_send_port);
+     address_send = lo_address_new_from_url(address_send_url);
+     if (!address_send) {
+         printf("Unable to open OSC URL for sending: %s\n", address_send_url);
+         exit(1);
+     }
+     printf("URL opened for sending: %s\n", address_send_url);
 
      try {
 
