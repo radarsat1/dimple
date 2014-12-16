88cd9499e082c6ac1d545f4f2a009604bdfe16fe
Stephen Sinclair <sinclair@music.mcgill.ca>
2014-12-16 17:30:46 -0500

    Remove extra add_simulation() call for visual/haptics connection.

--- src/dimple.cpp
+++ src/dimple.cpp
@@ -177,10 +177,6 @@ int main(int argc, char* argv[])
      if (visual && haptics)
          visual->add_simulation(*haptics);
 
-     // Visual can send a message to haptics due to keyboard
-     // shortcuts. (e.g. reset_workspace.)
-     visual->add_simulation(*haptics);
-
      // Interface can modify anything in
      // any other simulation.
      if (physics) interface.add_simulation(*physics);
