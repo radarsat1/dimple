018fa5ca449c7728ea31509692d047e07523234e
Stephen Sinclair <radarsat1@gmail.com>
2018-08-25 13:48:35 -0300

    Fix loader path to libusb dylib.

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -10,13 +10,16 @@ cd pages
 git checkout gh-pages
 cd ..
 
+DIMPLE=dimple-$TRAVIS_OS_NAME
+cp -rv install/bin/dimple pages/$DIMPLE
+echo $DIMPLE >>pages/index.html
+
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     ldd install/bin/dimple
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
-    otool -L install/bin/dimple
+    otool -L pages/$DIMPLE
     ls -l /usr/local/opt/libusb/lib/
-    cp -v /usr/local/opt/libusb/lib/libusb-1.0.0.dylib pages/)
+    cp -v /usr/local/opt/libusb/lib/libusb-1.0.0.dylib pages/
+    install_name_tool -change /usr/local/opt/libusb/lib/libusb-1.0.0.dylib "@loader_path/libusb-1.0.0.dylib" pages/$DIMPLE
+    otool -L pages/$DIMPLE
 fi
-
-cp -rv install/bin/dimple pages/dimple-`uname -s`
-echo dimple-`uname -s` >>pages/index.html
