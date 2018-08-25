341b1a5305b87802bf1e76988cd30af1bb9e3a66
Stephen Sinclair <radarsat1@gmail.com>
2018-08-25 13:52:47 -0300

    Ghpages script was renamed.

--- .travis-ghpages.sh
+++ .travis-ghpages.sh
@@ -5,17 +5,21 @@ find install
 echo find pages
 find pages
 
-if ! [ -d pages ]; then
-    mkdir -v pages
-fi
+git clone http://github.com/radarsat1/dimple.git pages
+cd pages
+git checkout gh-pages
+cd ..
+
+DIMPLE=dimple-$TRAVIS_OS_NAME
+cp -rv install/bin/dimple pages/$DIMPLE
+echo $DIMPLE >>pages/index.html
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     ldd install/bin/dimple
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
-    otool -L install/bin/dimple
+    otool -L pages/$DIMPLE
     ls -l /usr/local/opt/libusb/lib/
-    cp -v /usr/local/opt/libusb/lib/libusb-1.0.0.dylib pages/)
+    cp -v /usr/local/opt/libusb/lib/libusb-1.0.0.dylib pages/
+    install_name_tool -change /usr/local/opt/libusb/lib/libusb-1.0.0.dylib "@loader_path/libusb-1.0.0.dylib" pages/$DIMPLE
+    otool -L pages/$DIMPLE
 fi
-
-cp -rv install/bin/dimple pages/dimple-`uname -s`
-echo dimple-`uname -s` >>pages/index.html
