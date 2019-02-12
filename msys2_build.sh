08da69a9c7e19583a454a97821928299a8663a07
Stephen Sinclair <radarsat1@gmail.com>
2019-02-12 12:37:24 +0100

    wip try again running choco from yml

--- msys2_build.sh
+++ msys2_build.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-echo "msys2 build script"
-echo 0=\"$0\"
-pwd
-ls
-pacman -Ss zip unzip wget cmake patch
-which wget
-which zip
-which unzip
-which cmake
-which patch
-echo == /c/ProgramData/chocolatey/bin && ls /c/ProgramData/chocolatey/bin
-echo == /c/ProgramData/chocolatey/lib/mingw/tools/install && ls /c/ProgramData/chocolatey/lib/mingw/tools/install
-echo == /c/ProgramData/chocolatey/lib/mingw/tools/install/mingw64/bin && ls /c/ProgramData/chocolatey/lib/mingw/tools/install/mingw64/bin
-echo == /c/ && ls /c/
-echo == /c/tools && ls /c/tools
-echo == /c/tools/msys64 && ls /c/tools/msys64
-echo == /c/tools/msys64/bin && ls /c/tools/msys64/bin
-choco search msys2
-choco install -y msys2
-echo == /c/ && ls /c/
+echo "== msys2 build script"
+pacman -Ss automake
 exit 1
