678afb2811b7b3e0b83ec723cb98b13e7bbb812d
Stephen Sinclair <radarsat1@gmail.com>
2019-02-12 12:22:58 +0100

    check for msys64

--- msys2_build.sh
+++ msys2_build.sh
@@ -14,6 +14,9 @@ echo == /c/ProgramData/chocolatey/bin && ls /c/ProgramData/chocolatey/bin
 echo == /c/ProgramData/chocolatey/lib/mingw/tools/install && ls /c/ProgramData/chocolatey/lib/mingw/tools/install
 echo == /c/ProgramData/chocolatey/lib/mingw/tools/install/mingw64/bin && ls /c/ProgramData/chocolatey/lib/mingw/tools/install/mingw64/bin
 echo == /c/ && ls /c/
+echo == /c/tools && ls /c/tools
+echo == /c/tools/msys64 && ls /c/tools/msys64
+echo == /c/tools/msys64/bin && ls /c/tools/msys64/bin
 choco install msys2
 echo == /c/ && ls /c/
 exit 1
