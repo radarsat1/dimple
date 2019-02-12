b12eb8e1de09fa907b509bf2a4406a96f06df25c
Stephen Sinclair <radarsat1@gmail.com>
2019-02-12 12:50:20 +0100

    wip investigate paths after msys2 install

--- msys2_build.sh
+++ msys2_build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 echo "== msys2 build script"
-pacman -Ss automake
-exit 1
+which bash
+which gcc
+pacman -S msys/automake1.16 msys/autoconf
+pwd && bash ./bootstrap.sh && ./autogen.sh --prefix=$PWD/install && make && make check && make install && bash .travis-ghpages.sh || cat config.log
