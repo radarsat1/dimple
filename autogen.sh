a5561bbf3c42995b90411f89ec53c93b0c382ee8
Stephen Sinclair <radarsat1@gmail.com>
2019-03-29 15:02:04 +0100

    autogen.sh: Support an environment variable for --no-configure

--- autogen.sh
+++ autogen.sh
@@ -125,6 +125,14 @@ for arg in $@; do
     esac
 done
 
+if [ "x${NOCONFIGURE}" = x ]; then
+
 echo Running $srcdir/configure $conf_flags "$@" ...
 $srcdir/configure $conf_flags "$@" \
     && echo Now type \`make\' to compile. || exit 1
+
+else
+
+echo Skipping configure process.
+
+fi
